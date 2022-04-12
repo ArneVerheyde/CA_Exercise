58707050493fbdd590646991e2928d8c4d30a964
Giovanni Capocotta <r0875167@stud-r8-13.esat.kuleuven.be>
2022-04-12 20:52:22 +0200

    Session 1 part one WIP

--- SIM/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++ SIM/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -12,3 +12,17 @@
 1646813319 /users/students/r0875167/CA/CA_Exercise/RTL/sram.v
 1646813319 /users/students/r0875167/CA/CA_Exercise/RTL/cpu_tb.v
 1646813319 /users/students/r0875167/CA/CA_Exercise/RTL/saed32sram_mod.v
+1649775879 /users/students/r0875167/CA/current/RTL/alu.v
+1649775879 /users/students/r0875167/CA/current/RTL/alu_control.v
+1649768866 /users/students/r0875167/CA/current/RTL/branch_unit.v
+1649775879 /users/students/r0875167/CA/current/RTL/control_unit.v
+1649788796 /users/students/r0875167/CA/current/RTL/cpu.v
+1649768866 /users/students/r0875167/CA/current/RTL/immediate_extend_unit.v
+1649768866 /users/students/r0875167/CA/current/RTL/mux_2.v
+1649768866 /users/students/r0875167/CA/current/RTL/pc.v
+1649768866 /users/students/r0875167/CA/current/RTL/reg_arstn.v
+1649768866 /users/students/r0875167/CA/current/RTL/reg_arstn_en.v
+1649768866 /users/students/r0875167/CA/current/RTL/register_file.v
+1649768866 /users/students/r0875167/CA/current/RTL/sram.v
+1649768866 /users/students/r0875167/CA/current/RTL/cpu_tb.v
+1649768866 /users/students/r0875167/CA/current/RTL/saed32sram_mod.v
